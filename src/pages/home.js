17f44f5a7a90da4e0964c6dcac9266d7f34d16ad
mahvash <maghrabi.m@northeastern.edu>
2022-10-29 00:28:44 -0700

    React EC

---
+++ src/pages/home.js
@@ -0,0 +1,24 @@
+import React from "react";
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <div class="grid">
+          <header>
+            <h1>Mahvash's Website</h1>
+          </header>
+
+          <aside class="left"></aside>
+          <div class="content">
+            <div class="flex justify"></div>
+          </div>
+
+          <aside class="right"></aside>
+
+          <footer></footer>
+        </div>
+      </div>
+    );
+  }
+}
+export default Home;
